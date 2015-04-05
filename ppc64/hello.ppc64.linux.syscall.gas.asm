1ddf5e7245c3daa5cf253281bfec306fa2c159ec
matja <mat@gemnetworks.com>
2015-04-05 15:11:36 +0100

    Improve comments

--- ppc64/hello.ppc64.linux.syscall.gas.asm
+++ ppc64/hello.ppc64.linux.syscall.gas.asm
@@ -7,33 +7,33 @@ as -o hello.o hello.ppc64.linux.syscall.gas.asm &&
 ld -o hello hello.o
 */
 
-.data
+.section .rodata
 message:
 	.ascii "Hello world!\n"
 
-len = . - message
+message_length = . - message
 
-.text
+.section .text
 .global _start
 .section ".opd", "aw"
-.align 3
+.balign 8
 _start:
 .quad ._start, .TOC.@tocbase, 0
 .previous
 
 .global ._start
 ._start:
-	li 0, 4
-	li 3, 1
-	lis 4, message@highest
-	ori 4, 4, message@higher
+	li     0, 4 # syscall 4 - write
+	li     3, 1 # arg 0 - fd = 1 (stdout)
+	lis    4, message@highest # arg 1 - buffer
+	ori    4, 4, message@higher
 	rldicr 4, 4, 32, 31
-	oris 4, 4, message@h
-	ori 4, 4, message@l
-	li 5, len
-	sc
+	oris   4, 4, message@h
+	ori    4, 4, message@l
+	li     5, message_length # arg 2 - size
+	sc     # do syscall
 
-	li 0,1
-	li 3,1
-	sc
+	li     0, 1 # syscall 1 - exit
+	li     3, 0 # arg 0 - exit code = 0
+	sc     # do syscall
 
